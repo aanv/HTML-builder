127ce8c45d457b4804d4b626887481a6cec1b5de
aanv <volkauanton@gmail.com>
2021-11-08 01:22:38 +0300

    feat: 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const process = require('process');
+const readline = require('readline');
+
+let targetFilePath = path.join(__dirname, 'newText.txt');
+let readableStream = fs.createWriteStream(targetFilePath, 'utf-8');
+
+
+const rl = readline.createInterface({ 
+  input: process.stdin, 
+  output: process.stdout 
+});
+
+console.log('Enter your text:');
+
+rl.on('line', (input) => {
+  if (input === 'exit') {
+    rl.close();
+  } else {
+    readableStream.write(input + '\n');
+  }
+});
+
+rl.on('close', () => { 
+  console.log('\nThank you!'); 
+});
