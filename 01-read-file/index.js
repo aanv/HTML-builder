1476bbaddb1352878230844383b12d41e51310e2
aanv <volkauanton@gmail.com>
2021-11-09 02:19:06 +0300

    refactor: 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,6 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-let targetFilePath = path.join(__dirname, 'text.txt')
-
-let stream = fs.createReadStream(targetFilePath)
-
-stream.on('data', (data) => console.log(data.toString()))
+let targetFilePath = path.join(__dirname, 'text.txt');
+let stream = fs.createReadStream(targetFilePath);
+stream.on('data', (data) => console.log(data.toString()));
